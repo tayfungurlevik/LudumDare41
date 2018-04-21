fbffb3dc19d05aca670ede43778d6b8553a33552
tayfungurlevik <tgurlevik@gmail.com>
2018-04-21 19:41:35 +0300

    Created Camera and Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float rotationSpeed;
+    //private Animator animator;
+    
+    private void Awake()
+    {
+        //animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+    private void Move()
+    {
+        
+        var vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = transform.forward * vertical;
+        transform.position = transform.position + moveVector * moveSpeed * Time.deltaTime;
+        //var magnitude = Mathf.Abs(moveVector.magnitude);
+        //if (magnitude > 0.1)
+        //{
+        //    animator.SetFloat("Speed", magnitude);
+        //}
+        //else
+        //{
+        //    animator.SetFloat("Speed", 0);
+        //}
+        
+    }
+
+    private void Rotate()
+    {
+        var rotation = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * rotationSpeed * rotation*Time.deltaTime);
+    }
+
+}
